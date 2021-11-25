05f217fd5b230b914e1a0162428439af6641b963
Paritosh7 <paritosh7.sahni@gmail.com>
2021-11-25 17:21:15 +0530

    extension

--- testing/mock/monkey-patchin-ext.mjs
+++ testing/mock/monkey-patchin-ext.mjs
@@ -4,10 +4,19 @@ import thumbWar from "./thumb-war.mjs";
 test("returns winner", () => {
   const originalWinner = utils.getWinner;
 
-  utils.getWinner = (p1, p1) => p2;
+  utils.getWinner = (...args) => {
+    utils.getWinner.mock.calls.push(args);
+    return args[1];
+  };
+
+  utils.getWinner.mock = { calls: [] };
 
   const winner = thumbWar("a", "b");
   expect(winner).toBe("b");
+  expect(utils.getWinner.mock.calls).toHaveLength(2);
+  utils.getWinner.mock.calls.forEach((args) => {
+    expect(args).toEqual(["a", "b"]);
+  });
 
   utils.getWinner = originalWinner;
 });
